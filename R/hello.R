a39f64af7111d6cdf0e35604c441c109c6e18d61
Ishaan Dave <idave@uga.edu>
2019-06-18 16:12:03 -0400

    init add project files

---
+++ R/hello.R
@@ -0,0 +1,42 @@
+# Hello, world!
+
+
+dataProcessing = function (data, n) {
+  # getting variable names and number of unique levels in each of them
+  levelsEachVariable = data.frame(nLevels = double(),
+                                  var = character())
+
+  for (i in 1:ncol(data)){
+    levelsEachVariable[i,1] = length(unique(data[,i]))
+  }
+
+  levelsEachVariable$var = colnames(data)
+
+
+
+  simData <- data.frame(matrix(ncol = ncol(data), nrow = n))
+  colnames(simData) <- paste0("var", c(1:ncol(data)))
+
+
+  ## getting distribution of each variable and randomly sampling from that to get new dataset
+
+  for (i in 1:ncol(data))
+  {
+    set.seed(1234)
+    if (length(unique(data[,i])) < 5){
+      simData[,i] = sample(c(as.character(as.data.frame(table(data[,i]))$Var1)), n, TRUE,
+                           prob = c(as.data.frame(table(data[,i]))$Freq)
+      )
+    }
+    else  {
+      simData[,i] = round(rnorm(n, mean = mean(data[,i]), sd = sqrt(var(data[,i])) )
+      )
+    }
+    names(simData) = names(data)
+
+  } #close for loop
+  View(simData)
+  return(data.frame(simData))
+}
+
+dataProcessing(nodedata, 100)
