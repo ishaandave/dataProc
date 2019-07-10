a40b29cf95848f5e40c56dfeec35f1fd318a2342
Ishaan Dave <idave@uga.edu>
2019-07-10 18:01:01 -0400

    update

--- R/Untitled.R
+++ R/Untitled.R
@@ -1,5 +1,16 @@
-library(readxl)
-library(roxygen2)
+num.decimals <- function(x) {
+  stopifnot(class(x)=="numeric")
+  x <- nchar(sub("0+$","",sub("^.+[.]","",x)))
+  x <- sub("^.+[.]","",x)
+  nchar(x)
+}
+x <- 5.2300000
+num.decimals(x)
+
+5.1%%1
+5%%1
+5.12%%2
+
 dat = data.frame(read_xls("/Users/ishaandave/Desktop/SASUniversityEdition/myfolders/sasuser.v94/BIOS 8010/wcgs.xls"))
 dat2 = dat[c(1:20),]
 123
@@ -27,8 +38,12 @@ dates = as.data.frame(c(20141212,
 
 try = cbind(dat2, dates)
 names(try)[ncol(try)] = "dates"
-temp = try[c(1:6, 23),]
-temp = try[c(1:6), c(1:6,23)]
+
+
+
+
+# temp = try[c(1:6, 23),]
+# temp = try[c(1:6), c(1:6,23)]
 
 
 something = as.data.frame(cbind(try$arcus, try$chd69, try$smoke, try$typchd69))
