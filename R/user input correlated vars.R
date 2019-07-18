6f630a8418220bfa3e6f5b9b9bf62f8b2f4283f7
Dave <YHD8@cdc.gov>
2019-07-18 16:58:51 -0400

    update

--- R/user input correlated vars.R
+++ R/user input correlated vars.R
@@ -19,48 +19,50 @@ bivariateDistsList = function(indat, listVars, n) {
 
 
 
-    c1 = indat[,(listVars[i,1])]
+    c1 = indat[,(listVars[i,1])] ## creating 2 vectors from names of correlated variables
     c2 = indat[,(listVars[i,2])]
     c = cbind(c1, c2)
-    trueProbs = prop.table(table(c1, c2))
+    trueProbs = prop.table(table(c1, c2)) ## Getting "true" proportions from original dataset
     probmat = as.data.frame.matrix(trueProbs)
-    # rownames(probmat) = unique(all2$Geography)
-    # probmat2 = cbind(unique(all2$Geography), probmat)
-    names(probmat) = c(paste0("true", c(unique(c[,2]))))
+    names(probmat) = c(paste0("true", c(unique(c[,2]))))  ## Creating dataset of proportions from original datast
 
 
-
-
-    set.seed(1)
-    c1Sorted = sort(levels(factor(c1)))
+    c1Sorted = sort(levels(factor(c1))) ## sorting levels of the 2 vectors for convenience
     c2Sorted = sort(levels(factor(c2)))
-    n1 <- length(c1Sorted)
+    n1 <- length(c1Sorted)              ## Getting number of levels in each vector (used below)
     n2 <- length(c2Sorted)
 
-    probs  <- c(trueProbs)
-    events <- as.matrix(expand.grid(V1 = c1Sorted, V2 = c2Sorted))
-    samp   <- as.data.frame(events[sample.int(n1*n2, n, prob=probs, replace=TRUE),])
+    probs  <- c(trueProbs)              ## probability "weights" in a vector used for sampling
+    events <- as.matrix(expand.grid(V1 = c1Sorted, V2 = c2Sorted)) ## events is a matrix of all combinations of 2 variables
+    samp   <- as.data.frame(events[sample.int(n1*n2, n, prob=probs, replace=TRUE),]) ## sampling from combos with respective proportions
     head(samp)
 
-    predProbs = prop.table(table(samp[, 1], samp[, 2]))
-    predProbMat = as.data.frame(matrix(predProbs, ncol = nlevels(samp[,2])))
-    names(predProbMat) = paste0("pred", c(levels(samp$V2)))
+    predProbs = prop.table(table(samp[, 1], samp[, 2])) ## seeing new/sampled dataset proportions
+    predProbMat = as.data.frame(matrix(predProbs, ncol = nlevels(samp[,2]))) ## making a dataframe of sample proportions
+    names(predProbMat) = paste0("pred", c(levels(samp$V2))) ## changing column and row names for easy merging
     rownames(predProbMat) = c(c1Sorted)
 
 
 
     predProbMat2 = cbind(predProbMat, unique(samp$V1))
     names(predProbMat2) = c(paste0("pred", c(levels(samp$V2))), listVars[i,1])
-    predProbMat3 = predProbMat2[order(predProbMat2[,ncol(predProbMat2)]),]
+    predProbMat3 = predProbMat2[order(predProbMat2[,ncol(predProbMat2)]),] ## ordering for merging
 
-    observedAndPredicted = cbind(probmat, predProbMat3)
-    observedAndPredicted[is.na(observedAndPredicted)] <- 0
+    observedAndPredicted = cbind(probmat, predProbMat3) ## creating output dataset with true and predicted proportions
+                                                        ## to see how well the sampling procedure did
 
-   print(observedAndPredicted)
+    observedAndPredicted[is.na(observedAndPredicted)] <- 0 ## input 0's if a level of a variable wasn't sampled
+
+   print(observedAndPredicted) ## printing to check
 
   }
 
   return(observedAndPredicted)
+
+
+
+  simData[,indat[,(listVars[i,1])]] = samp$V1
+
 }
 
 
