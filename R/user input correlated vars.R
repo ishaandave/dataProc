35f90bbd7843758024ff1b53e5521e584cd7395b
Dave <YHD8@cdc.gov>
2019-07-18 16:06:36 -0400

    update

--- R/user input correlated vars.R
+++ R/user input correlated vars.R
@@ -15,8 +15,7 @@ corrVars = data.frame(read_xlsx("C:/Users/yhd8/Desktop/Data/Pretend/listVars.xls
 bivariateDistsList = function(indat, listVars, n) {
 
 
-  for (i in nrow(listVars)) {
-
+  for (i in 1:nrow(listVars)) {
 
 
 
@@ -28,7 +27,7 @@ bivariateDistsList = function(indat, listVars, n) {
     # rownames(probmat) = unique(all2$Geography)
     # probmat2 = cbind(unique(all2$Geography), probmat)
     names(probmat) = c(paste0("true", c(unique(c[,2]))))
-    print(probmat)
+
 
 
 
@@ -40,13 +39,13 @@ bivariateDistsList = function(indat, listVars, n) {
 
     probs  <- c(trueProbs)
     events <- as.matrix(expand.grid(V1 = c1Sorted, V2 = c2Sorted))
-    samp   <- as.data.frame(events[sample.int(n1*n2, 100, prob=probs, replace=TRUE),])
+    samp   <- as.data.frame(events[sample.int(n1*n2, n, prob=probs, replace=TRUE),])
     head(samp)
 
     predProbs = prop.table(table(samp[, 1], samp[, 2]))
     predProbMat = as.data.frame(matrix(predProbs, ncol = nlevels(samp[,2])))
     names(predProbMat) = paste0("pred", c(levels(samp$V2)))
-    rownames(predProbMat) = levels(samp$V1)
+    rownames(predProbMat) = c(c1Sorted)
 
 
 
@@ -56,9 +55,10 @@ bivariateDistsList = function(indat, listVars, n) {
 
     observedAndPredicted = cbind(probmat, predProbMat3)
     observedAndPredicted[is.na(observedAndPredicted)] <- 0
+   print(observedAndPredicted)
 
+  }
 
-    }
 
 }
 
@@ -67,55 +67,6 @@ bivariateDistsList = function(indat, listVars, n) {
 
 
 
-    set.seed(1)
-    c1Sorted = sort(levels(factor(c1)))
-    c2Sorted = sort(levels(factor(c2)))
-    n1 <- length(c1Sorted)
-    n2 <- length(c2Sorted)
-
-    probs  <- c(trueProbs)
-    events <- as.matrix(expand.grid(v1 = c1Sorted, v2 = c2Sorted))
-    nSamp  <- 50
-    samp   <- as.data.frame(events[sample.int(n1*n2, nSamp, prob=probs, replace=TRUE),])
-    head(samp)
-
-    predProbs = prop.table(table(samp$arcus, samp$behpat))
-    predProbMat = as.data.frame(matrix(predProbs, ncol = nlevels(samp$behpat)))
-    names(predProbMat) = paste0("pred", c(levels(samp$behpat)))
-    rownames(predProbMat) = levels(samp$arcus)
-
-
-
-    predProbMat2 = cbind(predProbMat, unique(samp$arcus))
-    names(predProbMat2) = c(paste0("pred", c(levels(samp$behpat))), "arcus")
-    predProbMat3 = predProbMat2[order(predProbMat2$arcus),]
-
-    observedAndPredicted = cbind(probmat, predProbMat3)
-    observedAndPredicted[is.na(observedAndPredicted)] <- 0
-
-    true = observedAndPredicted %>%
-      select(select_vars(names(observedAndPredicted), starts_with('true', ignore.case = TRUE)))
-
-    expected = observedAndPredicted %>%
-      select(select_vars(names(observedAndPredicted), starts_with('pred', ignore.case = TRUE)))
-
-    listTrue = c(as.matrix(true))
-    listExpected = c(as.matrix(expected))
-
-    plot(c(as.matrix(true)),
-         c(as.matrix(expected)), ylim = c(0, 1*max(c(listTrue, listExpected))), xlim = c(0,1*max(c(listTrue, listExpected))),
-         xlab = "True", ylab = "Expected")
-
-
-
-  }
-
-
-
-
-
-
-} ### END FUNCTION ###
 
 
 
@@ -131,11 +82,8 @@ bivariateDistsList = function(indat, listVars, n) {
 
 
 
-
-
-
-c1 = wcgs$arcus
-c2 = wcgs$behpat
+c1 = wcgs$smoke
+c2 = wcgs$agec
 c = cbind.data.frame(c1, c2)
 trueProbs = prop.table(table(c1, c2))
 probmat = as.data.frame.matrix(prop.table(table(c1, c2)))
@@ -160,15 +108,15 @@ samp   <- as.data.frame(events[sample.int(n1*n2, nSamp, prob=probs, replace=TRUE
 head(samp)
 
 predProbs = prop.table(table(samp$arcus, samp$behpat))
-predProbMat = as.data.frame(matrix(predProbs, ncol = nlevels(samp$behpat)))
-names(predProbMat) = paste0("pred", c(levels(samp$behpat)))
-rownames(predProbMat) = levels(samp$arcus)
+predProbMat = as.data.frame(matrix(predProbs, ncol = nlevels(c[,2])))
+names(predProbMat) = paste0("pred", c(levels(samp$v2)))
+rownames(predProbMat) = c(c1Sorted)
 
 
 
-predProbMat2 = cbind(predProbMat, unique(samp$arcus))
-names(predProbMat2) = c(paste0("pred", c(levels(samp$behpat))), "arcus")
-predProbMat3 = predProbMat2[order(predProbMat2$arcus),]
+predProbMat2 = cbind(predProbMat, unique(samp$v1))
+names(predProbMat2) = c(paste0("pred", c(levels(samp$v2))), "arcus")
+predProbMat3 = predProbMat2[order(predProbMat2[,ncol(predProbMat2)]),]
 
 observedAndPredicted = cbind(probmat, predProbMat3)
 observedAndPredicted[is.na(observedAndPredicted)] <- 0
