911cd2a612808999fbc3ddb68860bc14fc51bc74
Dave <YHD8@cdc.gov>
2019-07-19 15:44:21 -0400

    idk craziness

--- R/bivariate distributions.R
+++ R/bivariate distributions.R
@@ -1,8 +1,12 @@
-atl = read.csv("/Users/ishaandave/Desktop/CDC-Leidos/Data/Atlas/AtlasPlusTableData-nygafl16.csv")
-all = read.csv("/Users/ishaandave/Desktop/CDC-Leidos/Data/Atlas/AtlasPlusTableData-allstates2016.csv")
+# atl = read.csv("/Users/ishaandave/Desktop/CDC-Leidos/Data/Atlas/AtlasPlusTableData-nygafl16.csv")
+all = (read.csv("C:/Users/yhd8/Desktop/Data/Atlas/AtlasPlusTableData-allstates2016.csv", stringsAsFactors=FALSE))
+names(all)[1] = "state"
+all$Cases = as.numeric(all$Cases)
 
 
-# HELLO
+hhsRegions = readxl::read_xlsx("C:/Users/yhd8/Desktop/Data/Atlas/HHS Regions.xlsx")
+names(hhsRegions) = c("region", "division")
+
 # atl$Cases = as.numeric(gsub(",", "", atl$Cases))
 
 #
@@ -20,121 +24,137 @@ all = read.csv("/Users/ishaandave/Desktop/CDC-Leidos/Data/Atlas/AtlasPlusTableDa
 #
 #
 
-
+# allState = aggregate(all$Cases, by = list(all$Geography), FUN =  sum)
 all2 = all[all$Cases != "0" & !is.na(all$Age.Group) , ]
 
 
 all2$Cases = as.numeric(gsub(",","", all2$Cases))
 all3 = all2[!is.na(all2$Cases),]
+all3$stateAbb =  state.abb[match(as.character(all3$state),state.name)]
+
+# expand <- all3[rep(row.names(all3), all3$Cases), 1:4]
 
 
-expand <- all3[rep(row.names(all3), all3$Cases), 1:4]
+# fun = aggregate(expand, by = list(expand$Geography, expand$Race.Ethnicity), FUN = length, drop = T)
 
-fun = aggregate(expand, by = list(expand$Geography, expand$Race.Ethnicity), FUN = length, drop = T)
-
-
-
-c1 = expand$Geography
-c2 = expand$Race.Ethnicity
-c = cbind.data.frame(c1, c2)
-trueProbs = prop.table(table(c1, c2))
-probmat = data.frame(matrix(prop.table(table(c1, c2)), ncol = nlevels(c2)))
-# rownames(probmat) = unique(all2$Geography)
-probmat2 = cbind(unique(all2$Geography), probmat)
-names(probmat2) = c("State", levels(c2))
-
-
-
-
-
-set.seed(1)
-c1Sorted = sort(levels(c1))
-c2Sorted = sort(levels(c2))
-n1 <- length(c1Sorted)
-n2 <- length(c2Sorted)
-
-probs  <- c(trueProbs)
-events <- as.matrix(expand.grid(state = c1Sorted, race = c2Sorted))
-nSamp  <- 500000
-samp   <- as.data.frame(events[sample.int(n1*n2, nSamp, prob=probs, replace=TRUE),])
-head(samp)
-
-predProbs = prop.table(table(samp$state, samp$race))
-predProbMat = data.frame(matrix(predProbs, ncol = nlevels(samp$race)))
-
-predProbMat2 = cbind(unique(samp$state), predProbMat)
-names(predProbMat2) = c("State", levels(samp$race))
-predProbMat3 = predProbMat2[order(predProbMat2$State),]
-
-observedAndPredicted = left_join(x = probmat2, y = predProbMat3, by = "State")
-observedAndPredicted[is.na(observedAndPredicted)] <- 0
-
-true = observedAndPredicted %>%
-  select(select_vars(names(observedAndPredicted), ends_with('.x', ignore.case = TRUE)))
-
-expected = observedAndPredicted %>%
-  select(select_vars(names(observedAndPredicted), ends_with('.y', ignore.case = TRUE)))
-
-listTrue = c(as.matrix(true))
-listExpected = c(as.matrix(expected))
-plot(c(as.matrix(true)),
-     c(as.matrix(expected)), ylim = c(0, 1*max(c(listTrue, listExpected))), xlim = c(0,1*max(c(listTrue, listExpected))))
-
-
-fit = lm(log(listExpected)~log(listTrue))
-summary(fit)
-
-
-samp$stateAbb = state.abb[match(samp$state,state.name)]
+# fun$stateAbb = state.abb[match(as.character(fun$Group.1),state.name)]
 
 
 
 library(ggplot2)
 library(dplyr)
 
-us <- map_data("state")
+us_state_map = map_data("state");
+
+#map each state to a division
+us_state_map$division[us_state_map$region %in% c("connecticut", "maine", "massachusetts", "new hampshire", "rhode island", "vermont")] <- 1
+us_state_map$division[us_state_map$region %in% c("new jersey","new york","puerto rico","virgin islands")] <- 2
+us_state_map$division[us_state_map$region %in% c("delaware","district of columbia","maryland","pennsylvania","virginia","west virginia")] <- 3
+us_state_map$division[us_state_map$region %in% c("alabama","florida","georgia","kentucky","mississippi","north carolina","south carolina","tennessee")] <- 4
+us_state_map$division[us_state_map$region %in% c("illinois","indiana","michigan","minnesota","ohio","wisconsin")] <- 5
+us_state_map$division[us_state_map$region %in% c("arkansas","louisiana","new mexico","oklahoma","texas")] <- 6
+us_state_map$division[us_state_map$region %in% c("iowa","kansas","missouri","nebraska")] <- 7
+us_state_map$division[us_state_map$region %in% c("colorado","montana","north dakota","south dakota","utah","wyoming")] <- 8
+us_state_map$division[us_state_map$region %in% c("arizona","california","hawaii","nevada")] <- 9
+us_state_map$division[us_state_map$region %in% c("alaska","idaho","oregon","washington")] <- 10
+
+#create a dummy variable that counts the number of states in each division
+divisions.subtotal <- ddply(us_state_map, .(division), summarize, NumberOfStates=length(unique(region)))
+
+all3$state = tolower(all3$state)
+hhsRegions$region = tolower(hhsRegions$region)
+names(hhsRegions)[1] = "state"
+us_state_map = us_state_map[, -c(3, 4, 6)]
+f = merge(all3, hhsRegions, by = "state", all.x = T)
+names(f)[1] = "state"
+latLong = data.frame(state = (state.name),
+                     long = state.center$x,
+                     lat = state.center$y) %>%
+ right_join(f, by = "state")
 
 
-gg <- ggplot()
-gg <- gg + geom_map(data=us, map=us,
-                    aes(x=us$long, y=us$lat, map_id=us$region),
-                    fill="#ffffff", color="#ffffff", size=0.15)
-gg <- gg + geom_map(data=samp, map=us,
-                    aes(fill=allState$prop, map_id=allState$region),
-                    color="#ffffff", size=0.15)
-gg <- gg + scale_fill_continuous(low='thistle2', high='darkred',
-                                 guide='colorbar')
-gg <- gg + labs(x=NULL, y=NULL)
-gg <- gg + coord_map("albers", lat0 = 39, lat1 = 45)
-gg <- gg + theme(panel.border = element_blank())
-gg <- gg + theme(panel.background = element_blank())
-gg <- gg + theme(axis.ticks = element_blank())
-gg <- gg + theme(axis.text = element_blank())
-gg
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+new = aggregate(Cases ~ state, latLong, sum)
 
 
 
 
-blacks = expand[expand$Race.Ethnicity == "Black/African American",]
-
-allState = aggregate(all$Cases, by = list(all$Geography), FUN =  sum)
-allState$region = tolower(allState$Group.1)
-allState$prop = allState$x/sum(allState$x)
 
 
-allState$stateAbb = state.abb[match(allState$Group.1,state.name)]
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  ##################################################################################
+  ##################################################################################
+  ##################################################################################
+
+  ########################## USE THIS CODE FOR MAP OMG #############################
+
+  ##################################################################################
+  ##################################################################################
+  ##################################################################################
+
+
+  library(maps)
+  library(ggthemes)
+  names(new)[1] = "region"
+  new$region = tolower(new$region)
+  states_map <- map_data("state", region = new$region)
+  new_map <- merge(states_map, new, by = "region")
+  newHHS = merge(new_map, hhsRegions, by = "region"  )
+  new_map <- arrange(new_map, group, order) # to sort polygons in right order
+
+  ggplot(new_map, aes(x = long, y = lat, group = group, fill = Cases)) +
+    geom_polygon(color = "black") +
+    coord_map("polyconic") + theme_tufte() + labs(x = "", y = "")
+
+
+
+
+
+
+
+#
+# blacks = expand[expand$Race.Ethnicity == "Black/African American",]
+#
+# allState$region = tolower(allState$Group.1)
+# allState$prop = allState$x/sum(allState$x)
+#
+#
+# allState$stateAbb = state.abb[match(allState$Group.1,state.name)]
+#
